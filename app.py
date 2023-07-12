976e72103913606ee58d9e98b4cfb62321aec72b
nazaralievas <sadanaza4@gmail.com>
2023-07-13 01:52:38 +0600

    database added

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def homepage():
-    products = {'IPhone': 1100,
-                'Samsung': 650,
-                'Xiaomi': 450}
-    return render_template('homepage.html', products=products)
+    return render_template('homepage.html')
 
 @app.route('/about')
 def about():
@@ -15,6 +25,3 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# pip install flask-sqlalchemy
