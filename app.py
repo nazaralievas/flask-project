44db5a89208f9a827fbb70005e4288d258a95f1d
nazaralievas <sadanaza4@gmail.com>
2023-07-15 17:11:34 +0600

    final result

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+# начинаем создавать базу данных
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
+# описываем какие столбцы будут в таблице Todo
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String())
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+# этот код непосредственно создает таблицу в БД
 with app.app_context():
     db.create_all()
 
@@ -21,7 +26,8 @@ def homepage():
     tasks = Todo.query.all()
     if request.method == "POST":
         task_text = request.form['text']
-        new_task = Todo(text=task_text)
+        image = request.form['image']
+        new_task = Todo(text=task_text, image=image)
         db.session.add(new_task)
         db.session.commit()
         return redirect('/')
@@ -36,10 +42,22 @@ def delete(id):
     return redirect('/')
 
 
+@app.route('/update/<int:id>', methods=["POST", "GET"])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == "POST":
+       task_to_update.text = request.form['text']
+       task_to_update.image = request.form['image']
+       db.session.commit()
+       return redirect('/')
+    return render_template('update.html', task_to_update=task_to_update)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
+# этот код позволяет нам не перезапускать сервер после каждого изменения кода
 if __name__ == "__main__":
     app.run(debug=True)
